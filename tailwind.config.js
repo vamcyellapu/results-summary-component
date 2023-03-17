a334e9814d6a5741a157c61dfbcb4c939989a3c8
vamsi <vamcyellapu@gmail.com>
2023-03-17 18:47:59 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily:{
+      sans:['Hanken Grotesk','sans-serif']
+    },
+    extend: {
+      colors:{
+        lightRed:'hsl(0, 100%, 67%)',
+        orangeYellow:'hsl(39, 100%, 56%)',
+        greenTeal:'hsl(166, 100%, 37%)',
+        cobaltBlue: 'hsl(234, 85%, 45%)',
+        paleBlue:'hsl(221, 100%, 96%)',
+        lightLavender:'hsl(241, 100%, 89%)',
+        darkGrayBlue:'hsl(224, 30%, 27%)',
+        lightSlateBlue:'hsl(252, 100%, 67%)',
+        lightRoyalBlue:'hsl(241, 81%, 54%)',
+        violetBlue:'hsla(256, 72%, 46%, 1)',
+        persianBlue: 'hsla(241, 72%, 46%, 0)',
+
+      }
+    },
+  },
+  plugins: [],
+}
